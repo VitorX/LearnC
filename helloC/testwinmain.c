fd57c7cd2f85d4830ede9079e637e63350460218
Fei Xue (Shanghai Wicresoft Co Ltd) <v-feixue1@microsoft.com>
2024-03-21 18:52:24 +0800

    add test for using cl.exe

---
+++ helloC/testwinmain.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <commctrl.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    printf("hello WinMain() function!\n");
+    return 0;
+}
