793496e5811ded3655285f387a8a92de72ecd3ea
Fei Xue (Shanghai Wicresoft Co Ltd) <v-feixue1@microsoft.com>
2024-03-22 16:20:37 +0800

    fix issues

--- network/testwinet.c
+++ network/testwinet.c
@@ -11,7 +11,6 @@ int WINAPI Dumper( HINTERNET hResource);
 
 int wmain(int argc, wchar_t ** argv)
 {
-
     if(argc!=2)
     {
         printf("wrong usage!\n\tcommand {host}\n");
@@ -21,7 +20,7 @@ int wmain(int argc, wchar_t ** argv)
     HINTERNET internet = InternetOpen(L"WinInet Example/1.0",  INTERNET_OPEN_TYPE_PRECONFIG,NULL,NULL,0);
 
     if(internet)
-        internet = InternetConnect(internet,L"localhost234",INTERNET_DEFAULT_HTTP_PORT,NULL,NULL,INTERNET_SERVICE_HTTP,0,0);
+        internet = InternetConnect(internet,argv[1],INTERNET_DEFAULT_HTTP_PORT,NULL,NULL,INTERNET_SERVICE_HTTP,0,0);
     else
     {
         printf("InternetOpen failed (%d)!\n",GetLastError());
@@ -37,23 +36,26 @@ int wmain(int argc, wchar_t ** argv)
         return 1;
     }
 
-
     if(internet)
-        Dumper(internet);
-    else
+        HttpSendRequest(internet,NULL,0,NULL,0);
+     else
     {
         printf("HttpOpenRequest failed (%d)!\n",GetLastError());
         return 1;
     }   
 
-
-    printf("done");
-
+    if(internet)
+        Dumper(internet);
+    else
+    {
+        printf("HttpSendRequest failed (%d)!\n",GetLastError());
+        return 1;
+    }   
     return 0;
 }
 
 
-int WINAPI Dumper( HINTERNET hResource)
+int WINAPI Dumper(HINTERNET hResource)
 {
     LPTSTR lpszData;    // buffer for the data
     DWORD  dwSize;       // size of the data available
@@ -70,12 +72,12 @@ int WINAPI Dumper( HINTERNET hResource)
         if (!InternetQueryDataAvailable(hResource,&dwSize,0,0))
         {
             printf("InternetQueryDataAvailable failed (%d)\n", GetLastError());
-            SetCursor(LoadCursor(NULL,IDC_ARROW));
             return FALSE;
         }
         else
         {
-         
+            if(dwSize==0)
+                break;
             printf("data {%d} available\n",dwSize);
             // Allocate a buffer of the size returned by
             // InternetQueryDataAvailable.
@@ -95,62 +97,15 @@ int WINAPI Dumper( HINTERNET hResource)
             {
                 // Add a null terminator to the end of the data buffer
                 lpszData[dwDownloaded]='\0';
-
-                // Allocate the holding buffer.
-                lpszHolding = new TCHAR[dwSizeSum + dwDownloaded + 1];
-
-                // Check if there has been any data written
-                // to the textbox.
-                if (dwSizeSum != 0)
-                {
-                    // Add a null terminator at the end of the
-                    // textbox data.
-                    lpszHolding[dwSizeSum]='\0';
-                }
-                else
-                {
-                    // Make the holding buffer an empty string.
-                    lpszHolding[0]='\0';
-                }
-
-   printf("%s\n",lpszHolding);
-                size_t cchDest = dwSizeSum + dwDownloaded + dwDownloaded + 1;
-                LPTSTR* ppszDestEnd = 0;
-                size_t* pcchRemaining = 0;
-
-                // Add the new data to the holding buffer
-                HRESULT hr = StringCchCatEx(lpszHolding,
-                                            cchDest,
-                                            lpszData,
-                                            ppszDestEnd,
-                                            pcchRemaining,
-                                            STRSAFE_NO_TRUNCATION);
-
-                if(SUCCEEDED(hr))
-                {
-                    printf( "%s\n", lpszHolding );
-                    // Delete the two buffers.
-                    delete[] lpszHolding;
-                    delete[] lpszData;
-
-                    // Add the size of the downloaded data to the
-                    // textbox data size.
-                    dwSizeSum = dwSizeSum + dwDownloaded + 1;
-
-                    // Check the size of the remaining data.
-                    // If it is zero, break.
-                    if (dwDownloaded == 0)
+                printf("%s\n",lpszData);
+              
+                delete[] lpszData;
+                   if (dwDownloaded == 0)
                         break;
-                    else
-                    {
-                    //  TODO: Insert error handling code here.
-                    }
-                }
             }
         }
     }
     while(TRUE);
-    system("pause");
     // Close the HINTERNET handle.
     InternetCloseHandle(hResource);
 
