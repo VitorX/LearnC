b6998b8ba3da388e2a13be8c8027fd47567d8fb3
Fei Xue (Shanghai Wicresoft Co Ltd) <v-feixue1@microsoft.com>
2024-03-28 09:35:18 +0800

    update intersectLinkedList sample

--- leecode/intersectLinkedlist.c
+++ leecode/intersectLinkedlist.c
@@ -1,84 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct listNode{
+struct listNode
+{
     int val;
     struct listNode *next;
 };
 
-void printNode(const char*name ,struct listNode *pNode)
+void printNode(const char *name, struct listNode *pNode)
 {
-    if(pNode==NULL)
-        printf("%s,NULL\t",name);
+    if (pNode == NULL)
+        printf("%s,NULL\t", name);
     else
-        printf("%s:%d\t",name,pNode->val);
+        printf("%s:%d\t", name, pNode->val);
 }
-struct listNode* getIntersectionNode(struct listNode *headA,struct listNode *headB)
+struct listNode *getIntersectionNode(struct listNode *headA, struct listNode *headB)
 {
-    struct listNode *pHeadA=headA;
-    struct listNode *pHeadB=headB;
+    struct listNode *pHeadA = headA;
+    struct listNode *pHeadB = headB;
 
-    while(pHeadA!=pHeadB||(pHeadA->next==NULL && pHeadB->next==NULL))
+    while ((pHeadA != pHeadB) && (pHeadA->next != NULL && pHeadB->next != NULL))
     {
-        if(pHeadA->next)
-            pHeadA=pHeadA->next;
+        if (pHeadA->next)
+            pHeadA = pHeadA->next;
         else
-            pHeadA=headB;
-        if(pHeadB->next)
-            pHeadB=pHeadB->next;
+            pHeadA = headB;
+        if (pHeadB->next)
+            pHeadB = pHeadB->next;
         else
-            pHeadB=headA;
+            pHeadB = headA;
     }
-    return pHeadA;
+    if (pHeadA == pHeadB)
+        return pHeadA;
+    else
+        return NULL;
 }
 
 void test1()
-{  
+{
     struct listNode node1;
-    node1.val=1;
-    node1.next=NULL;
+    node1.val = 1;
+    node1.next = NULL;
 
-    struct listNode *headA=&node1;
-    struct listNode *headB=&node1;
-    struct listNode *intersectionNode= getIntersectionNode(headA,headB);
+    struct listNode *headA = &node1;
+    struct listNode *headB = &node1;
+    struct listNode *intersectionNode = getIntersectionNode(headA, headB);
 
-    if(intersectionNode)
-        printf("intersectionNode node value: %d\n",intersectionNode->val);
+    if (intersectionNode)
+        printf("intersectionNode node value: %d\n", intersectionNode->val);
     else
         printf("no intersection!\n");
 }
 
 void test2()
-{  
+{
     struct listNode node1;
-    node1.val=2;
+    node1.val = 2;
     struct listNode node2;
-    node1.val=6;
+    node2.val = 6;
     struct listNode node3;
-    node1.val=4;
+    node3.val = 4;
     struct listNode node4;
-    node1.val=1;
+    node4.val = 1;
     struct listNode node5;
-    node1.val=5;
-    node1.next=&node2;
-    node2.next=&node3;
-    node3.next=NULL;
-    node4.next=&node5;
-    node5.next=NULL;
+    node5.val = 5;
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;
+    node4.next = &node5;
+    node5.next = NULL;
 
-    struct listNode *headA=&node1;
-    struct listNode *headB=&node4;
-    struct listNode *intersectionNode= getIntersectionNode(headA,headB);
+    struct listNode *headA = &node1;
+    struct listNode *headB = &node4;
+    struct listNode *intersectionNode = getIntersectionNode(headA, headB);
 
-    if(intersectionNode)
-        printf("intersectionNode node value: %d\n",intersectionNode->val);
+    if (intersectionNode)
+        printf("intersectionNode node value: %d\n", intersectionNode->val);
     else
         printf("no intersection!\n");
 }
 
-
-
-int main(int argc,char** argv)
+int main(int argc, char **argv)
 {
     /*
     if(NULL)
