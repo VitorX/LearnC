206d343753731bc471938fc1e813f2cd247c376b
Fei Xue <fxnotepad@gmail.com>
2024-04-17 15:44:19 +0800

    update 7.21

--- c-faq/7/7.21.c
+++ c-faq/7/7.21.c
@@ -6,10 +6,13 @@ int main(int argc,char** argv)
 {
 	int *p=malloc(sizeof(int));
 	*p=10;
-	printf("allocate int at the address %p\n",p);
+	int *q=p;
+	printf("allocate int at the address %p, and the value is %d.\n",p,*p);
 	free(p);
 	if(p==NULL)
 		printf("the point p is freed sucessfully!\n");
 	else
-		printf("the address of p is:%p after call free()\n",p);
+		printf("the address of p is:%p after call free(), and the value is %d.\n",p,*p);
+
+	printf("%p--->%d\n",q,*q);
 }
