f0bb810ec19a9e2b66b8fe8f7b15aca8e26e7bce
Fei Xue <fxnotepad@gmail.com>
2024-06-29 10:23:11 +0800

    update struct test sample

--- basic/struct.c
+++ basic/struct.c
@@ -12,5 +12,14 @@ struct data_st
 int main()
 {
     struct data_st myst;
+	myst.i=10;
+	myst.f=20.0f;
+	myst.c='A';
     printf("sizeof(data_st): %ld\n",sizeof(myst));
+	printf("%p----->i=%d,f=%f,c=%c\n",&myst,myst.i,myst.f,myst.c);
+struct data_st *p;
+p=malloc(sizeof(*p));
+*p=myst;
+	printf("%p----->i=%d,f=%f,c=%c\n",p,myst.i,myst.f,myst.c);
+
 }
